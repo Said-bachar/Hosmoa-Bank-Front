6dcecdf1361d219f2c3c523e911e3123894f11b0
Haitham Oumerzoug <haithamoumerzoug31@gmail.com>
2021-06-11 14:29:10 +0000

    [dev] :haitham :add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { ClientNavbarComponent } from './components/partials/client-navbar/client-navbar.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { ClientAgentComponent } from './components/client-agent/client-agent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    ClientNavbarComponent
+    ClientNavbarComponent,
+    AccountsComponent,
+    ClientAgentComponent
   ],
   imports: [
     BrowserModule,
