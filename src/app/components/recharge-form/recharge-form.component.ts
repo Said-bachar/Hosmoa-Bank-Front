49b276d3dd4a9eec1a86dd3ba620611a26f24714
Haitham Oumerzoug <haithamoumerzoug31@gmail.com>
2021-06-12 17:41:37 +0000

    [dev] haitham: add custom recharge confirm UI

--- src/app/components/recharge-form/recharge-form.component.ts
+++ src/app/components/recharge-form/recharge-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-recharge-form',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RechargeFormComponent implements OnInit {
    
+  rechargeForm=new FormGroup({
+    account:new FormControl(''),
+    key:new FormControl(''),
+    operator:new FormControl(''),
+    phone:new FormControl(''),
+    amount:new FormControl('')
+  })
   credentialsVerified = false;
  
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
@@ -17,5 +26,43 @@ export class RechargeFormComponent implements OnInit {
   next() {
     this.credentialsVerified = true;
   }
+  pred(){
+    this.credentialsVerified = false;
+  }
 
+  async recharge(){
+    const { value: key } = await Swal.fire({
+      title: 'Enter your Key secret',
+      input: 'password',
+      inputLabel: 'Key Secret',
+      inputPlaceholder: 'Enter your Key secret',
+      showCancelButton: true,
+      confirmButtonColor:'#29A3DD',
+      confirmButtonText: 'Confirm recharge',
+      cancelButtonColor:'#E95314',
+      inputValidator: (value) => {
+        return new Promise((resolve) => {
+          if (!value) {
+            resolve('You need to enter you Key Secret :)')
+          }else {
+            console.log(this.rechargeForm.value)
+            //Call the regarge service
+            Swal.fire({
+              icon: 'success',
+              title: 'Gooooooood!',
+              text: 'Your recharge has been done ;)',
+              timer:2000
+            })
+          }
+        })
+      }
+    });
+
+   
+    
+  }
 }
+
+
+      
+
