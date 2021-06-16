cc6dc32949cfe09ddaef502b937b0c39bd046050
Said <saidbachar.uca.ma@gmail.com>
2021-06-16 18:52:49 +0100

    [dev said]: adding somme services & models

---
+++ src/app/models/account.model.ts
@@ -0,0 +1,11 @@
+export interface Account {
+
+  accountNumber?: string;
+  intitule: string;
+  solde: number;
+  dateOperation: string;
+  lastOperation: Date;
+  statut: string;
+  oldStatut: string;
+  dateCreation: Date;
+}
